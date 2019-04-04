78c451936af54e23d07be3be0232570f60ac5e8b
zztttt <1315691850@qq.com>
2019-04-04 21:36:30 +0800

    update 91

---
+++ 091.DecodeWays/README.md
@@ -0,0 +1,18 @@
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+```
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+```
+
+Example
+```
+Input: "12"
+Output: 2
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+
+Input: "226"
+Output: 3
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+```
---
+++ 091.DecodeWays/solution.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<stack>
+#include<vector>
+#include<set>
+#include<map>
+
+using namespace std;
+class Solution {
+public:
+	int numDecodings(string s) {
+		int len = s.length();
+		if (s[0] == '0')
+			return 0;
+		/*
+		* 0 0
+		* 1 1
+		* 2 2
+		* 3 3
+		* i
+		*/
+		//init. dp[i] indicates the number of way to decode s[0, i-1];
+		vector<int> dp(len + 1, 0);
+		dp[0] = 1; dp[1] = 1;
+		for (int i = 2; i < len + 1; ++i) {
+			//single code
+			if (s[i - 1] >= '1' && s[i - 1] <= '9')
+				dp[i] = dp[i] + dp[i - 1];
+			//two word code
+			if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] >= '0' && s[i - 1] <= '6'))
+				dp[i] = dp[i] + dp[i - 2];
+		}
+		return dp[len];
+	}
+};
+int main() {
+	string s = "226";
+	int ret = Solution().numDecodings(s);
+	system("pause");
+}
