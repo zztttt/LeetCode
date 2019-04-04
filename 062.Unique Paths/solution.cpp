e65c848b984690f7e99da653898d1e9f6455c82b
zztttt <1315691850@qq.com>
2019-04-04 21:07:28 +0800

    update 62

---
+++ 062.Unique Paths/solution.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<stack>
+#include<vector>
+#include<set>
+#include<map>
+
+using namespace std;
+
+class Solution {
+public:
+	int uniquePaths(int m, int n) {
+		/*  0 1 2 m 
+		* 0 1 1 1
+		* 1 1 2 3
+		* n
+		* 
+		*/
+		//init dp array
+		vector<vector<int>> dp(n, vector<int>(m, 0));
+		for (int i = 0; i < n; ++i) {
+			dp[i][0] = 1;
+		}
+		for (int j = 0; j < m; ++j) {
+			dp[0][j] = 1;
+		}
+		//dp[i][j] = dp[i-1][j]+dp[i][j-1]
+		for (int i = 1; i < n; ++i) {
+			for (int j = 1; j < m; ++j) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+		return dp[n - 1][m - 1];
+	}
+};
+int main() {
+	int m = 3, n = 2;
+	int ret = Solution().uniquePaths(m, n);
+	system("pause");
+}
