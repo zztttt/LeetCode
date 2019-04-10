955ffa1b16783508f6e0d7d32a2f2c6aa4e82ed5
zztttt <1315691850@qq.com>
2019-04-10 22:49:31 +0800

    update 456

---
+++ 456.132Pattern/solution.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<stack>
+#include<vector>
+#include<set>
+#include<map>
+#include<string>
+#include<sstream>
+
+using namespace std;
+
+class Solution {
+public:
+	bool find132pattern(vector<int>& nums) {
+		int tmp = INT_MIN;
+		stack<int> stk;
+		for (int i = nums.size() - 1; i >= 0; --i) {
+			if (nums[i] < tmp)
+				return true;
+			else while (!stk.empty() && nums[i] > stk.top()) {
+				tmp = stk.top();
+				stk.pop();
+			}
+			stk.push(nums[i]);
+		}
+		return false;
+	}
+};
+int main() {
+	
+	vector<int> w = { 2, 3, 1, 1, 4 };
+	bool ret = Solution().find132pattern(w);
+	system("pause");
+}
