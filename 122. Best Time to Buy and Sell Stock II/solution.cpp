8f2ff93c7e195301be8d1abea5a39ed401f04090
zztttt <1315691850@qq.com>
2019-05-06 15:06:12 +0800

    update 122

---
+++ 122. Best Time to Buy and Sell Stock II/solution.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<stack>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+#include<string>
+#include<sstream>
+#include<assert.h>
+using namespace std;
+
+/*
+* Runtime: 8 ms, faster than 99.21% of C++ online submissions for Best Time to Buy and Sell Stock II.
+* Memory Usage: 9.8 MB, less than 5.39% of C++ online submissions for Best Time to Buy and Sell Stock II.
+*/
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		int ret = 0, len = prices.size();
+		if (len == 0) return ret;
+		for (int i = 1; i < len; ++i) {
+			if (prices[i] > prices[i - 1])
+				ret = ret + prices[i] - prices[i - 1];
+		}
+		return ret;
+	}
+};
+
+int main() {
+	vector<int> v = { 7, 1, 5, 3, 6, 4 };
+	int ret = Solution().maxProfit(v);
+	return 0;
+}
