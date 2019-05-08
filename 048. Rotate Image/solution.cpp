9a7a877750ab9da56ec9ac17b9499d573da4627d
zztttt <1315691850@qq.com>
2019-05-08 19:42:48 +0800

    update 48

---
+++ 048. Rotate Image/solution.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<map>
+#include<vector>
+using namespace std;
+/*
+* Runtime: 4 ms, faster than 100.00% of C++ online submissions for Rotate Image.
+* Memory Usage: 9.1 MB, less than 94.85% of C++ online submissions for Rotate Image.
+*/
+class Solution
+{
+public:
+	void rotate(vector<vector<int>> &m){
+		vector<vector<int>> tmp = m;
+		int row = m.size();
+		if (row == 0)return;
+		int col = m[0].size();
+		for (int i = 0; i < row; ++i) {
+			for (int j = 0; j < col; ++j) {
+				m[i][j] = tmp[col - j - 1][i];
+			}
+		}
+	}
+};
+
+int main() {
+	
+}
+
