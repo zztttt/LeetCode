e24d5e79024f25e7e9353046875d733a5b70c307
zztttt <1315691850@qq.com>
2020-02-03 14:37:46 +0800

    update 131

---
+++ 131. Palindrome Partitioning/solution.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ret;
+        vector<string> cur;
+        
+        partition(ret, cur, s);
+        
+        return ret;
+    }
+    
+    void partition(vector<vector<string>>& ret, vector<string>& cur, string s){
+        int len = s.size();
+        if(len == 0){
+            ret.push_back(cur);
+            return;
+        }
+            
+        for(int i = 0; i < len; ++i){
+            // s[0, i]
+            string tmp = s.substr(0, i + 1);
+            if(isPalindrome(tmp)){
+                cur.push_back(tmp);
+                // s[i + 1, len - 1]
+                partition(ret, cur, s.substr(i + 1, len - i));
+                // backtrace
+                cur.pop_back();
+            }
+        }
+    }
+    
+    bool isPalindrome(string s){
+        int len = s.size();
+        if(len == 0 || len == 1)
+            return true;
+        int low = 0, high = len - 1;
+        while(low <= high){
+            if(s[low] != s[high])
+                return false;
+            low++;
+            high--;
+        }
+        return true;
+    }
+};
