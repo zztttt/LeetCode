93811b2bb6fcf0c52f2fbaf476cef6d584abaa07
zztttt <1315691850@qq.com>
2020-02-15 21:48:14 +0800

    update 142

---
+++ 142. Linked List Cycle II/twopoints.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == nullptr)
+            return nullptr;
+
+        ListNode *fast = head, *slow = head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow)
+                break;
+        }
+
+        if(fast == nullptr || fast->next == nullptr)
+            return nullptr;
+        fast = head;
+        // [1,2] 0
+        while(fast != slow){
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        return fast;
+    }
+};
