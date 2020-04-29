eee2f1c3dc7aeba8f029f5229bcc076c384951ee
zztttt <1315691850@qq.com>
2020-04-29 13:17:24 +0800

    update day29

---
+++ Thirty-Day LeetCoding Challenge/Week 4/First Unique Number/sol.cpp
@@ -0,0 +1,47 @@
+class FirstUnique {
+public:
+    vector<int> uniques;
+    map<int, int> m;
+    FirstUnique(vector<int>& v) {
+        for(int i: v){
+            map<int, int>::iterator it = m.find(i);
+            if(it == m.end()){
+                // unique
+                uniques.push_back(i);
+                m[i] = 1;
+            }else{
+                // duplicate
+                m[i]++;
+            }
+        }
+    }
+    
+    int showFirstUnique() {
+        for(int i: uniques){
+            map<int, int>::iterator it = m.find(i);
+            if(it != m.end() && it->second == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    void add(int i) {
+        map<int, int>::iterator it = m.find(i);
+        if(it == m.end()){
+            // unique
+            uniques.push_back(i);
+            m[i] = 1;
+        }else{
+            // duplicate
+            m[i]++;
+        }
+    }
+};
+
+/**
+ * Your FirstUnique object will be instantiated and called as such:
+ * FirstUnique* obj = new FirstUnique(nums);
+ * int param_1 = obj->showFirstUnique();
+ * obj->add(value);
+ */
