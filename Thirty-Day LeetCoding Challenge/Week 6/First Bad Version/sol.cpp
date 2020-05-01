8dbd6b1a0ec8fb7f0908e698aaf4452bff418b15
zztttt <1315691850@qq.com>
2020-05-01 20:44:41 +0800

    update day1

---
+++ Thirty-Day LeetCoding Challenge/Week 6/First Bad Version/sol.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 0, high = n;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(isBadVersion(mid)){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return isBadVersion(low)? low : low + 1;
+    }
+};
