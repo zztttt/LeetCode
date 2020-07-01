79017d705e9fc2d0bf3bef1fb47893ce4325d1bc
zztttt <1315691850@qq.com>
2020-07-01 23:54:48 +0800

    update 1 java version

---
+++ 0001.TwoSum/sol.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int ret[] = new int[2];
+        for(int i = 0; i < len; ++i){
+            map.put(nums[i], i);
+        }
+        for(int i = 0; i < len; ++i){
+            int dst = target - nums[i];
+            if(map.containsKey(dst) && map.get(dst) != i){
+                ret[0] = i;
+                ret[1] = map.get(dst);
+                return ret;
+            }
+        }
+        return ret;
+    }
+}
