32d026d7c21edc23b3977699a2840c0521dd783d
zztttt <1315691850@qq.com>
2021-11-19 19:05:17 +0800

    update 461

--- 0461.Hamming Distance/bit.cpp
+++ 0461.Hamming Distance/bit.cpp
@@ -1,1 +1,18 @@
-Hamming Distance
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<bool> bitmap(n + 1, 0);
+        for(int i: nums){
+            bitmap[i] = true;
+        }
+        
+        vector<int> ret;
+        for(int i = 1; i < n + 1; ++i){
+            if(!bitmap[i])
+                ret.push_back(i);
+        }
+        return ret;
+    }
+};
