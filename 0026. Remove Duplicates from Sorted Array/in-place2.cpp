729fd77c87c3b85193cf5535213e7c5abed756f9
zztttt <1315691850@qq.com>
2022-08-05 22:23:13 +0800

    update 26

---
+++ 0026. Remove Duplicates from Sorted Array/in-place2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& v) {
+        if(v.size() == 0)
+            return 0;
+        int len = v.size(), index = 1, last = v[0];
+        for(int i = 1; i < len; ++i){
+            if(v[i] != last){
+                v[index++] = v[i];
+                last = v[i];
+            }
+        }
+        
+        return index;
+    }
+};
